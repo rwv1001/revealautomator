1c2f68f3d14474e50e39d8b5ebb5d178399a87da
Robert Verrill <robertverrill@Roberts-Air.lan>
2021-09-20 12:04:00 +0100

    setup of the express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,19 @@
 console.log('Hello')
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express()
+app.use(morgan('combined'))
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world!'
+    })
+})
+
+app.listen(process.env.PORT)
