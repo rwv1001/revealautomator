444af604b350604b47a5ab90300868bff3f45a40
Robert Verrill <robertverrill@Roberts-Air.lan>
2021-09-20 14:17:55 +0100

    setup of registration button

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Register from '@/components/Register'
 const routes = [
   {
     path: '/',
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   }
 ]
 
--- server/src/app.js
+++ server/src/app.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world!'
+        message: `Hello ${req.body.email}! your user was registered`
     })
 })
 
